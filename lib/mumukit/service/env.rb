290517674375bd19aad1d8d6ddbdd235fd39b6e0
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2017-02-21 01:19:38 -0300

    Removing depracated variables

--- lib/mumukit/service/env.rb
+++ lib/mumukit/service/env.rb
@@ -4,14 +4,6 @@ module Mumukit::Service::Env
       ENV['MUMUKI_ATHENEUM_URL']
     end
 
-    def atheneum_client_secret
-      ENV['MUMUKI_ATHENEUM_CLIENT_SECRET']
-    end
-
-    def atheneum_client_id
-      ENV['MUMUKI_ATHENEUM_CLIENT_ID']
-    end
-
     def bot_username
       ENV['MUMUKI_BOT_USERNAME']
     end
@@ -23,14 +15,5 @@ module Mumukit::Service::Env
     def bot_api_token
       ENV['MUMUKI_BOT_API_TOKEN']
     end
-
-    def auth0_client_id
-      ENV['MUMUKI_AUTH0_CLIENT_ID']
-    end
-
-    def auth0_client_secret
-      ENV['MUMUKI_AUTH0_CLIENT_SECRET']
-    end
-
   end
 end
